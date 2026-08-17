9ddf4cdc84d8bf4922fea8d2cefef1b95fffad56
Valentin <vyotov07@gmail.com>
2026-08-17 09:25:42 +0300

    creating vector db

---
+++ main.py
@@ -0,0 +1,26 @@
+from langchain_ollama.llms import OllamaLLM
+from langchain_core.prompts import ChatPromptTemplate
+
+
+model = OllamaLLM(model = "llama3.2")
+
+template = """
+You are an expert in answerting questions about a pizza restaurant
+
+Here are some relevant reviews: {reviews}
+
+Here is the question: {question}
+"""
+
+prompt = ChatPromptTemplate.from_template(template)
+
+chain = prompt | model
+
+while True:
+    question = input("Ask a question about the pizza restaurant(q to quit): ")
+
+    if question.lower() == "q":
+        break
+
+    result = chain.invoke({ "reviews":[], "question":question })
+    print(result)
